9cf580e1c81e7416b3844eb525635b37097a8209
saidone <saidone@saidone.org>
2021-05-07 11:30:38 +0200

    fn to get favourite language from request

--- src/clj/latakia/components/dictionary.clj
+++ src/clj/latakia/components/dictionary.clj
@@ -4,3 +4,12 @@
 
 (defn get-dictionary [params]
   (:dictionaries (config/config)))
+
+(defn get-favourite-language [request]
+  (val
+   (first
+    (reduce
+     #(let [[l q] (s/split %2 #";")]
+        (assoc %1 (if (nil? q) 1.0 (Float/parseFloat(last (s/split q #"=")))) l))
+     (sorted-map-by >)
+     (s/split (get (:headers request) "accept-language") #"\,")))))
